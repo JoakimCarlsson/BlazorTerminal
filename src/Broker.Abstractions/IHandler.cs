1fa3c1c7c0ebf7f68141a6eec3a6e64ae9bb9289
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2023-07-29 09:36:28 +0200

    Initial commit

---
+++ src/Broker.Abstractions/IHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Broker.Abstractions;
+
+public interface IHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+}
+
+public interface IHandler<in TRequest> where TRequest : IRequest
+{
+    Task HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+}
+
+public interface IRequest { }
+
+public interface IRequest<out TResponse> : IRequest { }
---
+++ src/Broker.Abstractions/ISender.cs
@@ -0,0 +1,10 @@
+﻿namespace Broker.Abstractions;
+
+public interface ISender
+{
+    Task SendAsync<TRequest>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest;
+
+    Task<TResponse> SendAsync<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken = default)
+        where TRequest : IRequest<TResponse>;
+}
