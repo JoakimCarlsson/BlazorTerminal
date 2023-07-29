bf9bdfb1acaa9addc53719d33f6f78d9ac10af99
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2023-07-30 00:37:04 +0200

    Words 4 dayz

--- src/BlazorTerminal.Api/Feature/Game/Create/CreateGameHandler.cs
+++ src/BlazorTerminal.Api/Feature/Game/Create/CreateGameHandler.cs
@@ -20,7 +20,7 @@ internal sealed class CreateGameHandler : IHandler<CreateGameCommand, Results<Cr
         CancellationToken cancellationToken = default)
     {
         var random = new Random();
-        IEnumerable<string> scrambledWords = new List<string> { "word1", "word2", "word3", "word4", "word5" };
+        var scrambledWords = GenerateWords(10, 5).ToList();
         
         var gameSession = new GameSession(
             Guid.NewGuid(),
@@ -49,5 +49,37 @@ internal sealed class CreateGameHandler : IHandler<CreateGameCommand, Results<Cr
                 createdSession.AttemptsRemaining
             )
         );
+    }
+    
+    //get a list of words from somewhere else :-)
+    private IEnumerable<string> GenerateWords(int wordCount, int wordLength)
+    {
+        var allWords = GetWords();
+
+        var suitableWords = allWords.Where(word => word.Length == wordLength).ToList();
+        var random = new Random();
+
+        var selectedWords = new List<string>();
+        for (var i = 0; i < wordCount; i++)
+        {
+            var randomIndex = random.Next(suitableWords.Count);
+            selectedWords.Add(suitableWords[randomIndex]);
+            suitableWords.RemoveAt(randomIndex);
+        }
+
+        return selectedWords;
+    }
+
+    private IEnumerable<string> GetWords()
+    {
+        return new List<string> {
+            "ABOUT", "AFTER", "APPLE", "ARISE", "BASIC", "BLAME", "BRICK", "CHAIR", "CHIEF", "CHILD",
+            "CLAIM", "CLASS", "CLEAR", "CLOCK", "COULD", "CROSS", "DANCE", "DRINK", "EARTH", "EIGHT",
+            "EQUAL", "ERROR", "FINAL", "FIRST", "FIXED", "FLAME", "FRESH", "FRUIT", "GLASS", "GUESS",
+            "HEART", "HUMAN", "INDEX", "ISSUE", "JUDGE", "KNIFE", "LEARN", "LEGAL", "LEVEL", "LOWER",
+            "MAGIC", "MAJOR", "MIGHT", "NEVER", "NOISE", "OCCUR", "OTHER", "POWER", "QUICK", "RIVER",
+            "SCALE", "SILENCE", "SINCE", "SWEET", "THANK", "THEIR", "THIRD", "THOSE", "UNDER", "USAGE",
+            "VALUE", "VIDEO", "WATCH", "WORLD", "WORTH", "YOUTH", "ZEBRA", "ALLOW", "ALONG", "BELOW"
+        };
     }
 }
