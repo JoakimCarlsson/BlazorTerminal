48a3e22f6679d3ef648780a0700bd3595cd051a4
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2023-07-31 20:58:44 +0200

    fix

--- src/BlazorTerminal.Api/Program.cs
+++ src/BlazorTerminal.Api/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCosmosDb("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+builder.Services.AddCosmosDb("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="); //todo fix this
 builder.Services.AddBroker();
 builder.Services.AddCors();
 builder.Services.AddDistributedMemoryCache();
