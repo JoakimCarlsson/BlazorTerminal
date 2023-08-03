fb18cf98f59a55ee187e550f75a5e31175dd70ac
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2023-08-03 22:22:51 +0200

    Source generator refactoring.

--- src/Broker.SourceGenerator/GlobalUsings.cs
+++ src/Broker.SourceGenerator/GlobalUsings.cs
@@ -1,5 +1,11 @@
 ﻿global using System.Linq;
 global using System.Text;
+global using System.CodeDom.Compiler;
+global using System.IO;
+global using System.Globalization;
+global using System.Collections.Generic;
+
+global using Broker.SourceGenerator.Extensions;
 
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
