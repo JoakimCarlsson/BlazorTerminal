67e47ccd228f00bc30008775d6e96e743b4a7062
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2023-08-06 21:16:03 +0200

    Add logging to CosmosInitializationService
    Introduced a logger into the CosmosInitializationService to provide better error feedback when initializing Cosmos DB. This change also wraps the ExecuteAsync method body under a try-catch block to handle potential exceptions during initialization and log them. This makes error tracing easier and enables prompt identification of Cosmos DB initialization issues.

--- src/BlazorTerminal.Api/Persistence/Services/CosmosInitializationService.cs
+++ src/BlazorTerminal.Api/Persistence/Services/CosmosInitializationService.cs
@@ -3,34 +3,47 @@
 internal sealed class CosmosInitializationService : BackgroundService
 {
     private readonly CosmosClient _cosmosClient;
+    private readonly ILogger<CosmosInitializationService> _logger;
 
-    public CosmosInitializationService(CosmosClient cosmosClient)
+    public CosmosInitializationService(
+        CosmosClient cosmosClient,
+        ILogger<CosmosInitializationService> logger
+        )
     {
         _cosmosClient = cosmosClient;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var partitionKeyPath = $"/{nameof(GameSession.Id).ToLower()}";
-        
-        var containerProperties = new ContainerProperties(CosmosGlobals.GameSessionContainerName, partitionKeyPath)
+        try
         {
-            IndexingPolicy = new IndexingPolicy
-            {
-                Automatic = true,
-                IndexingMode = IndexingMode.Consistent,
-                ExcludedPaths = { new ExcludedPath { Path = "/*" } }
-            }
-        };
-        
-        var database = await _cosmosClient.CreateDatabaseIfNotExistsAsync(
-            CosmosGlobals.DatabaseName,
-            cancellationToken: stoppingToken
-        );
+            var partitionKeyPath = $"/{nameof(GameSession.Id).ToLower()}";
 
-        await database.Database.CreateContainerIfNotExistsAsync(
-            containerProperties,
-            cancellationToken: stoppingToken
-        );
+            var containerProperties = new ContainerProperties(CosmosGlobals.GameSessionContainerName, partitionKeyPath)
+            {
+                IndexingPolicy = new IndexingPolicy
+                {
+                    Automatic = true,
+                    IndexingMode = IndexingMode.Consistent,
+                    ExcludedPaths = { new ExcludedPath { Path = "/*" } }
+                }
+            };
+
+            var database = await _cosmosClient.CreateDatabaseIfNotExistsAsync(
+                CosmosGlobals.DatabaseName,
+                cancellationToken: stoppingToken
+            );
+
+            await database.Database.CreateContainerIfNotExistsAsync(
+                containerProperties,
+                cancellationToken: stoppingToken
+            );
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error initializing Cosmos DB: {Message}", e.Message);
+            throw;
+        }
     }
 }
