b138921e992e8c1d3b1d4523a8acdd35b47650b2
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2023-08-08 14:00:30 +0200

    Update UI to create a terminal-like experience.

--- src/BlazorTerminal.Client/Pages/Index.razor.cs
+++ src/BlazorTerminal.Client/Pages/Index.razor.cs
@@ -14,4 +14,40 @@ public partial class Index : IDisposable
     public void Dispose() => _cancellationTokenSource.Dispose();
     
     protected override void OnInitialized() => GameSessionStore.OnChange += StateHasChanged;
+    
+    private RenderFragment GenerateTerminalText()
+    {
+        return builder =>
+        {
+            var messages = new List<string>
+            {
+                "Bypassing the mainframe...",
+                "Decrypting enemy cipher...",
+                "Overriding security protocols...",
+                "Synchronizing the neuro-algorithm...",
+                "Scanning network topology...",
+                "Establishing secure uplink...",
+                "Injecting payload into target's database...",
+                "Retrieving encrypted data packets...",
+                "Uploading trojan horse...",
+                "Disabling enemy firewalls...",
+                "Activating dark web channels...",
+                "Initializing stealth VPN connection...",
+                "Compiling next-gen decompiler...",
+                "Intercepting server handshake signals...",
+                "Randomizing IP address...",
+                "Navigating through digital minefield...",
+                "Hacking into the mainframe...",
+            };
+
+            var random = new Random();
+            var numberOfLines = random.Next(10, messages.Count - 1);
+
+            var sequence = 0;
+            for (var i = 0; i < numberOfLines; i++)
+            {
+                builder.AddContent(sequence++, messages[random.Next(messages.Count)] + "\n");
+            }
+        };
+    }
 }
